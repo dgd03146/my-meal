b957e5f0fb26002eb2dd9f7e415a8bdd2873c021
dgd03146 <ibory1220@naver.com>
2023-10-08 00:13:11 +0100

    feat: Add user in sanity database #2

---
+++ src/service/user.ts
@@ -0,0 +1,27 @@
+import { client } from './sanity';
+
+type OAuthUser = {
+  id: string;
+  email: string;
+  name: string;
+  username: string;
+  image?: string | null;
+};
+
+export async function addUser({ id, username, email, name, image }: OAuthUser) {
+  return client
+    .createIfNotExists({
+      _id: id,
+      _type: 'user',
+      username,
+      email,
+      name,
+      image,
+      following: [],
+      followers: [],
+      bookmarks: [],
+    })
+    .then((res) => {
+      console.error(`${res.username} was created`);
+    });
+}
