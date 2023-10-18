0443ee24d203fdea77eb4cc851e1a9bc7f51b727
dgd03146 <ibory1220@naver.com>
2023-10-18 19:44:48 +0100

    feat: Implement SEO #23

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -2,11 +2,15 @@ import UserProfile from '@/components/UserProfile';
 import UserPosts from '@/components/UserPosts';
 import { getUserForProfile } from '@/service/user';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+import { cache } from 'react';
 
 type Props = { params: { username: string } };
 
+const getUser = cache(async (username: string) => getUserForProfile(username));
+
 export default async function UserPage({ params: { username } }: Props) {
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
 
   if (!user) {
     notFound();
@@ -18,4 +22,12 @@ export default async function UserPage({ params: { username } }: Props) {
       <UserPosts user={user} />
     </section>
   );
+}
+
+export async function generateMetadata({ params: { username } }: Props): Promise<Metadata> {
+  const user = await getUser(username);
+  return {
+    title: `${user?.name} (@${user?.username}) · Instantgram Photos`,
+    description: `${user?.name}'s all Instantgram posts`,
+  };
 }
