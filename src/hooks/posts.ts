e049c7b1d4afe5ed48e1b8c36c89eec45b9f69a0
dgd03146 <ibory1220@naver.com>
2023-10-19 17:10:41 +0100

    feat: Implement optimitic like button #25

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,16 +1,41 @@
 import useSWR, { useSWRConfig } from 'swr';
 import { SimplePost } from '@/model/post';
+import { useCallback } from 'react';
+
+async function updateLike(id: string, like: boolean) {
+  return fetch('/api/likes', {
+    method: 'PUT',
+    body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
 
 export default function usePosts() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
-  const { mutate } = useSWRConfig();
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<SimplePost[]>('/api/posts');
 
-  const setLike = (post: SimplePost, username: string, like: boolean) => {
-    fetch('api/likes', {
-      method: 'PUT',
-      body: JSON.stringify({ id: post.id, like }),
-    }).then(() => mutate('/api/posts'));
-  };
+  const setLike = useCallback(
+    (post: SimplePost, username: string, like: boolean) => {
+      const newPost = {
+        ...post,
+        likes: like
+          ? [...post.likes, username]
+          : post.likes.filter((item) => item !== username),
+      };
+      const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+      return mutate(updateLike(post.id, like), {
+        optimisticData: newPosts,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      });
+    },
+    [posts, mutate],
+  );
 
   return { posts, isLoading, error, setLike };
 }
